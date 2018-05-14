fae8940ac1f91d792b8a5b916ce81b20748f85cd
Vu Anh <anhv.ict91@gmail.com>
2018-05-14 10:17:18 +0700

    add code

--- train.py
+++ train.py
@@ -6,19 +6,24 @@ from util.convert_to_fasttext_classification_corpus import convert_to_fasttext_c
 
 parser = argparse.ArgumentParser("train.py")
 parser.add_argument("--train", help="train file", required=True)
+parser.add_argument("--cross-validation", type=int, help="cross validation")
 args = parser.parse_args()
 
 path = os.path.abspath(args.train)
-convert_to_fasttext_classification_corpus(path, "tmp/train.txt")
 
-classifier = fasttext.supervised('tmp/train.txt', 'tmp/model.bin')
-sentences = [
-    'Tôi bị Phạm Anh Khoa khóa tay, ép vào tường để tấn công tình dục',
-    'Người Thái cử thêm 3 nhân sự vào Bia Sài Gòn',
-    'Salah dập tắt tham vọng của Real Madrid',
-    'Tottenham có vé Champions League, đẩy Liverpool xuống thứ tư',
-    'Nadal phá kỷ lục tồn tại 34 năm, gặp Thiem ở tứ kết Madrid Mở rộng',
-    'Diễn biến ngày đầu The Players Championship',
-    'Xuân Trường lọt top 5 bàn thắng đẹp nhất vòng 7 V-League 2018'
-]
-print(classifier.predict(sentences))
+if args.cross_validation:
+    print(args.cross_validation + 1)
+
+# convert_to_fasttext_classification_corpus(path, "tmp/train.txt")
+#
+# classifier = fasttext.supervised('tmp/train.txt', 'tmp/model.bin')
+# sentences = [
+#     'Tôi bị Phạm Anh Khoa khóa tay, ép vào tường để tấn công tình dục',
+#     'Người Thái cử thêm 3 nhân sự vào Bia Sài Gòn',
+#     'Salah dập tắt tham vọng của Real Madrid',
+#     'Tottenham có vé Champions League, đẩy Liverpool xuống thứ tư',
+#     'Nadal phá kỷ lục tồn tại 34 năm, gặp Thiem ở tứ kết Madrid Mở rộng',
+#     'Diễn biến ngày đầu The Players Championship',
+#     'Xuân Trường lọt top 5 bàn thắng đẹp nhất vòng 7 V-League 2018'
+# ]
+# print(classifier.predict(sentences))
