8df99ff21d7fbc236d4415254992515a6b1f36e7
Vu Anh <anhv.ict91@gmail.com>
2018-06-02 16:12:24 +0700

    update train script

--- train.py
+++ train.py
@@ -1,4 +1,6 @@
 import argparse
+
+from languageflow.evaluation import print_cm
 from os.path import join
 import os
 import fasttext
@@ -16,29 +18,6 @@ parser.add_argument("--train-test-split", type=float,
 parser.add_argument("-s", help="path to save model")
 parser.add_argument("--cross-validation", type=int, help="cross validation")
 args = parser.parse_args()
-
-def print_cm(cm, labels, hide_zeroes=False, hide_diagonal=False, hide_threshold=None):
-    """pretty print for confusion matrixes"""
-    columnwidth = max([len(x) for x in labels] + [5])  # 5 is value length
-    empty_cell = " " * columnwidth
-    # Print header
-    print("    " + empty_cell, end=" ")
-    for label in labels:
-        print("%{0}s".format(columnwidth) % label, end=" ")
-    print()
-    # Print rows
-    for i, label1 in enumerate(labels):
-        print("    %{0}s".format(columnwidth) % label1, end=" ")
-        for j in range(len(labels)):
-            cell = "%{0}.1f".format(columnwidth) % cm[i, j]
-            if hide_zeroes:
-                cell = cell if float(cm[i, j]) != 0 else empty_cell
-            if hide_diagonal:
-                cell = cell if i != j else empty_cell
-            if hide_threshold:
-                cell = cell if cm[i, j] > hide_threshold else empty_cell
-            print(cell, end=" ")
-        print()
 
 
 if args.mode == "train":
