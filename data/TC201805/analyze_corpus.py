4c7d42756227d02c4fea673da4a98cddbcfdc4c8
Vu Anh <anhv.ict91@gmail.com>
2018-08-15 14:15:06 +0700

    delete tc201805 dataset

--- data/TC201805/analyze_corpus.py
+++
@@ -1,7 +0,0 @@
-f = open("topic_classifier_ver1.0.txt")
-for i, line in enumerate(f):
-    x = line.find(" ")
-    label = line[:x]
-    text = line[x+1:]
-    if len(text) < 3:
-        print(i, ",", text)
