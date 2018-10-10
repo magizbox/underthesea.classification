f98f8a1e21b58d1b991308eb4f35073b434f4817
Bùi Nhật Anh <buinhatanh1208@gmail.com>
2018-10-10 14:42:38 +0700

    save model

---
+++ classification.py
@@ -0,0 +1,29 @@
+import argparse
+
+from models import classifier
+
+parser = argparse.ArgumentParser("classification.py")
+text = parser.add_argument_group("The following arguments are mandatory for text option")
+text.add_argument("text", metavar="TEXT", help="text to predict", nargs="?")
+file = parser.add_argument_group("The following arguments are mandatory for file option")
+file.add_argument("--fin", help="text file input")
+args = parser.parse_args()
+
+if not (args.text or args.fin):
+    parser.print_help()
+
+if args.text:
+    text = args.text
+    predict = classifier(text)[0]
+    label = predict.replace("_", " ").capitalize()
+    print(label)
+
+if args.fin:
+    if not args.fin:
+        parser.error("File option requires --fin and --fout")
+    fin = args.fin
+    with open(fin) as f:
+        text = f.read()
+    predict = classifier(text)[0]
+    label = predict.replace("_", " ").capitalize()
+    print(label)
