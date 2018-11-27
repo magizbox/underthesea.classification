24e5315dbe949d95f2b454b63df66149ece577b1
Vu Anh <anhv.ict91@gmail.com>
2018-11-27 12:01:58 +0700

    add lm

---
+++ egs/vntc_lm/language_model_classifier.py
@@ -0,0 +1,14 @@
+class LanguageModelClassifier:
+    def __init__(self):
+        self.lms = []
+
+    def fit(self, corpus_path):
+        pass
+
+    def predict(self, file):
+        pass
+
+
+if __name__ == '__main__':
+    clf = LanguageModelClassifier()
+    clf.fit()
