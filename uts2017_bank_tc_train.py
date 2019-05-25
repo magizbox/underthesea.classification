b19f028346a44c9d5011c231a2e02e4f6954521d
QuangHPham <hongquang2310255@gmail.com>
2019-05-26 00:51:39 +0700

    GH-55: edit uts2017_bank_tc train

--- uts2017_bank_tc_train.py
+++ uts2017_bank_tc_train.py
@@ -1,5 +1,6 @@
+import os
 import shutil
-from tempfile import mkdtemp
+import time
 
 from languageflow.data import CategorizedCorpus
 from languageflow.data_fetcher import DataFetcher, NLPData
@@ -10,23 +11,36 @@ from sklearn.metrics import f1_score
 from sklearn.pipeline import Pipeline
 from sklearn.svm import SVC
 
+model_folder = "tmp/classification_svm_uts2017_bank"
+try:
+    shutil.rmtree(model_folder)
+except:
+    pass
+finally:
+    os.makedirs(model_folder)
 
+count__ngram_range = (1, 3)
+count__max_features = 4000
+estimator__C = 0.3
+
+start = time.time()
+print(">>> Train UTS2017_BANK Classification")
 corpus: CategorizedCorpus = DataFetcher.load_corpus(NLPData.UTS2017_BANK_TC)
+print("\n\n>>> Sample sentences")
+for s in corpus.train[:10]:
+    print(s)
 pipeline = Pipeline(
-    steps=[('features', CountVectorizer(ngram_range=(1, 3), max_features=4000)),
-           ('estimator', SVC(kernel='linear', C=0.3))]
+    steps=[('features', CountVectorizer(ngram_range=count__ngram_range, max_features=count__max_features)),
+           ('estimator', SVC(kernel='linear', C=estimator__C))]
 )
 classifier = TextClassifier(estimator=TEXT_CLASSIFIER_ESTIMATOR.PIPELINE, pipeline=pipeline)
 model_trainer = ModelTrainer(classifier, corpus)
-tmp_model_folder = mkdtemp()
-
 
 
 def macro_f1_score(y_true, y_pred):
     return f1_score(y_true, y_pred, average='macro')
 
 
-score = model_trainer.train(tmp_model_folder, scoring=macro_f1_score)
-print(score)
-
-shutil.rmtree(tmp_model_folder)
+model_trainer.train(model_folder, scoring=macro_f1_score)
+print(f"\n\n>>> Finish training in {round(time.time() - start, 2)} seconds")
+print(f"Your model is saved in {model_folder}")
