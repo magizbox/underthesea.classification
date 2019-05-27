7b57868adcaaddd8418068f9bd3c20271ea23170
QuangHPham <hongquang2310255@gmail.com>
2019-05-27 22:36:38 +0700

    GH-54: update folder structure

--- egs/vntc_lm/evaluate.py
+++
@@ -1,7 +0,0 @@
-import os
-from os.path import join, dirname
-
-srilm_path = "/usr/share/srilm/bin/i686-m64"
-
-model_path = join(dirname(__file__), "tmp")
-model_name = [i for i in os.listdir(model_path) if i.endswith(".bin")]
