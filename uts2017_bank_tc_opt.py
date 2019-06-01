6f02120a4b1690d94b48b892bd2fa104c367a779
Quang Pham <Quangphampm@gmail.com>
2019-06-01 09:08:14 +0700

    GH-55: update UTS2017_BANK_TC hyper parameters (#62)

--- uts2017_bank_tc_opt.py
+++ uts2017_bank_tc_opt.py
@@ -10,35 +10,52 @@ from languageflow.trainers.model_trainer import ModelTrainer
 from sacred import Experiment
 from sacred.observers import MongoObserver
 from sacred.optional import np
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.metrics import f1_score
-from sklearn.pipeline import Pipeline
+from sklearn.pipeline import Pipeline, FeatureUnion
 from sklearn.svm import SVC
 
-ex = Experiment('ust2017_bank_tc_opt_2605')
+from text_features import Lowercase, RemoveTone, CountEmoticons
+
+ex = Experiment('bank_31')
 ex.observers.append(MongoObserver.create())
 
 
 @ex.main
-def my_run(estimator__C, features__ngram_range, features__max_features):
+def my_run(estimator__C,
+           features__lower_pipe__tfidf__max_features,
+           features__lower_pipe__tfidf__ngram_range,
+           features__with_tone_char__ngram_range,
+           features__remove_tone__tfidf__ngram_range):
     params = locals().copy()
     start = time.time()
     print(params)
     corpus: CategorizedCorpus = DataFetcher.load_corpus(NLPData.UTS2017_BANK_TC)
     pipeline = Pipeline(
-        steps=[('features', CountVectorizer()),
-               ('estimator', SVC(kernel='linear'))
-               ]
+        steps=[
+            ('features', FeatureUnion([
+                ('lower_pipe', Pipeline([
+                    ('lower', Lowercase()),
+                    ('tfidf', TfidfVectorizer(norm='l2', min_df=2))])),
+                ('with_tone_char', TfidfVectorizer(norm='l2', min_df=2, analyzer='char')),
+                ('remove_tone', Pipeline([
+                    ('remove_tone', RemoveTone()),
+                    ('lower', Lowercase()),
+                    ('tfidf', TfidfVectorizer(norm='l2', min_df=2))])),
+                ('emoticons', CountEmoticons())
+            ])),
+            ('estimator', SVC(kernel='linear', class_weight=None, verbose=True))
+        ]
     )
     pipeline.set_params(**params)
     classifier = TextClassifier(estimator=TEXT_CLASSIFIER_ESTIMATOR.PIPELINE, pipeline=pipeline)
     model_trainer = ModelTrainer(classifier, corpus)
     tmp_model_folder = "tmp/tmp_model"
 
-    def macro_f1_score(y_true, y_pred):
-        return f1_score(y_true, y_pred, average='macro')
+    def micro_f1_score(y_true, y_pred):
+        return f1_score(y_true, y_pred, average='micro')
 
-    score = model_trainer.train(tmp_model_folder, scoring=macro_f1_score)
+    score = model_trainer.train(tmp_model_folder, scoring=micro_f1_score)
     tmp_files = listdir(tmp_model_folder)
     for file in tmp_files:
         if "gitignore" in file:
@@ -63,13 +80,19 @@ def objective(space):
 
 space = {
     'estimator__C': hp.choice('estimator__C', np.arange(0.01, 0.9, 0.01)),
-    'features__ngram_range': hp.choice('features__ngram_range', [(1, 2), (1, 3), (1, 4)]),
-    'features__max_features': hp.choice('features__max_features', np.arange(1000, 6000, 1000))
+    'features__lower_pipe__tfidf__max_features': hp.choice('features__lower_pipe__tfidf__max_features',
+                                                           np.arange(1000, 10000, 1000)),
+    'features__lower_pipe__tfidf__ngram_range': hp.choice('features__lower_pipe__lower_tfidf__ngram_range',
+                                                          [(1, 3), (1, 4), (1, 5), (1, 6)]),
+    'features__with_tone_char__ngram_range': hp.choice('features__with_tone_char__ngram_range',
+                                                       [(1, 3), (1, 4), (1, 5), (1, 6)]),
+    'features__remove_tone__tfidf__ngram_range': hp.choice('features__remove_tone__tfidf__ngram_range',
+                                                           [(1, 3), (1, 4), (1, 5), (1, 6)])
 }
 
 start = time.time()
 trials = Trials()
-best = fmin(objective, space=space, algo=tpe.suggest, max_evals=1000, trials=trials)
+best = fmin(objective, space=space, algo=tpe.suggest, max_evals=500, trials=trials)
 
-print(f"Hyperopt search took {round(time.time() - start, 2)} seconds for 1000 candidates")
+print(f"Hyperopt search took {round(time.time() - start, 2)} seconds for 500 candidates")
 print(-best_score, best)
